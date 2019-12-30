f972125f481d5953f8210b45094219e671d2c448
Jens Nilsson <jens.nilsson@leovegas.com>
2019-12-30 01:10:50 +0100

    Day 14, part 1

--- src/main/kotlin/day14/day14.kt
+++ src/main/kotlin/day14/day14.kt
@@ -9,25 +9,23 @@ fun partTwo(input : Map<String, Pair<Int, List<Pair<Int,String>>>>) : Int {
 }
 
 fun getQuantity(input : Map<String, Pair<Int, List<Pair<Int,String>>>>, startResource : String) : Int {
-    val required = input.keys.map { it to 0 }.toMap().toMutableMap()
-    required[startResource] = input[startResource]?.first ?: error("No way this can happen!")
+    val required = input.keys.map { it to
+            if (it == startResource) input[startResource]?.first ?: 0 else 0
+    }.toMap().toMutableMap()
     var ore = 0
-    var needMoreResources = true
-    while (needMoreResources) {
-        val key = required.toList().firstOrNull { it.second > 0 }
-        needMoreResources = key?.let { key ->
-            val needed = input[key.first] ?: error("Can't happen either!")
-            required[key.first] = required[key.first]?.minus(needed.first)!!
-            needed.second.forEach { resource ->
-                val resourceIsNotOre = required[resource.second]
-                resourceIsNotOre?.let {
-                    required[resource.second] = resource.first + resourceIsNotOre
-                } ?: run {
-                    ore += resource.first
-                }
+    var key = required.toList().firstOrNull { it.second > 0 }
+    while (key != null) {
+        val needed = input[key.first] ?: error("Can't happen!")
+        required[key.first] = required[key.first]?.minus(needed.first) ?: error("Can't happen!")
+        needed.second.forEach { resource ->
+            val resourceIsNotOre = required[resource.second]
+            resourceIsNotOre?.let {
+                required[resource.second] = resource.first + resourceIsNotOre
+            } ?: run {
+                ore += resource.first
             }
-            true
-        } ?: false
+        }
+        key = required.toList().firstOrNull { it.second > 0 }
     }
     return ore
 }
